5ffaa932ed8e74beb9376fb754c533b4c56cbbdf
Amael Tardif <amael.tardif@epita.fr>
2019-02-27 19:53:30 +0100

    move read-csv to parser

--- company/src/main/scala/Parser.scala
+++ company/src/main/scala/Parser.scala
@@ -1,8 +1,19 @@
 import play.api.libs.json.Json
 import scala.io.Source
 
-def parseJson(file: String):Void = {
-    val fileContents = Source.fromFile(file).getLines.mkString
-    val parsed = Json.parse(fileContents)
-    println(parsed)
+class Parser(dir: String) {
+    def parseJson(file: String): Unit = {
+        val fileContents = Source.fromFile(file).getLines.mkString
+        val parsed = Json.parse(fileContents)
+        println(parsed)
+    }
+    def parseCSV(file: String): Unit = {
+        println("Index, Girth, Height, Volume")
+        val bufferedSource = Source.fromFile("example.csv")
+        for (line <- bufferedSource.getLines) {
+            val cols = line.split(",").map(_.trim)
+            println(s"${cols(0)} | ${cols(1)} | ${cols(2)} | ${cols(3)}")
+        }
+        bufferedSource.close
+    }
 }
--- company/src/main/scala/read-csv.scala
+++
@@ -1,9 +0,0 @@
-object CSVDemo extends App {
-    println("Index, Girth, Height, Volume")
-    val bufferedSource = io.Source.fromFile("example.csv")
-    for (line <- bufferedSource.getLines) {
-        val cols = line.split(",").map(_.trim)
-        println(s"${cols(0)} | ${cols(1)} | ${cols(2)} | ${cols(3)}")
-    }
-    bufferedSource.close
-}
