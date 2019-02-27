095748efda2d36457c9eb0b2adb0d09f0164bb8f
Amael Tardif <amael.tardif@epita.fr>
2019-02-27 20:36:11 +0100

    continue file reading

--- company/src/main/scala/Parser.scala
+++ company/src/main/scala/Parser.scala
@@ -1,5 +1,6 @@
 import play.api.libs.json.Json
 import scala.io.Source
+import java.io.File
 
 class Parser(dir: String) {
     def parseJson(file: String): Unit = {
@@ -15,5 +16,25 @@ class Parser(dir: String) {
             println(s"${cols(0)} | ${cols(1)} | ${cols(2)} | ${cols(3)}")
         }
         bufferedSource.close
+    }
+
+    def listAllFiles(path: String): List[File] = {
+        val directory = new File(path)
+        if (directory.exists && directory.isDirectory) {
+            directory.listFiles.toList
+        }
+        else {
+            List[File]()
+        }
+    }
+
+    def listDirectories(path: String): List[File] = {
+        listAllFiles(path).filter(_.isDirectory)
+    }
+
+    def listFiles(path: String): List[String] = {
+        listAllFiles(path).filter{
+          f => f.isFile && (f.getName.endsWith(".csv")
+            || f.getName.endsWith(".json")) }.map(_.getAbsolutePath)
     }
 }
