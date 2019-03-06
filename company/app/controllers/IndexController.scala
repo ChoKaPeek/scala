a9e0f64eaef33338a6b16b95cf368fbdba16678c
Xavier Cadet <xavier.cadet@epita.fr>
2019-03-06 19:40:59 +0100

    play_controllers: add IndexController

---
+++ company/app/controllers/IndexController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+
+class IndexController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+    def index() = Action {
+      Ok("Hello, World!")
+    }
+}
