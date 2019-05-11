30e1d21c4dd7f74af84252413885d1a819d1adba
Xavier Cadet <xavier.cadet@epita.fr>
2019-05-11 12:16:53 +0200

    handler: controller msg msg save with dull json validation

--- handler/app/controllers/IndexController.scala
+++ handler/app/controllers/IndexController.scala
@@ -3,11 +3,17 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.mvc._
+import play.api.libs.json._
+
 import play.api.libs.functional.syntax._
 
-case class Drone(id: Int, speed: Float, altitude: Float, latitude: Float,
-                 longitude: Float, datetime: String, temperature: Float,
+case class Drone(id: Int, speed: Float, altitude: Float, latitude: Double,
+                 longitude: Double, datetime: String, temperature: Float,
                  battery: Float)
+
+
 
 class IndexController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController {
@@ -32,12 +38,31 @@ class IndexController @Inject()(val controllerComponents: ControllerComponents)
       Ok(views.html.hello("Welcome to DroneTech", user, posts))
     }
 
-    def msg() = Action {request =>
-        implicit val droneReads = Json.reads[Drone]
-        val json = request.body.asJson.get
-        val drone = json.as[Drone]
-        fixme = drone :: fixme;
-        println(drone)
-        Ok
+    implicit object DroneReads extends Reads[Drone] {
+        def reads(json: JsValue) = JsSuccess(Drone(  // Has to be a JsResult, should be a case with the possibility of a JsFailure
+            (json \ "id").as[Int],
+            (json \ "speed").as[Float],
+            (json \ "altitude").as[Float],
+            (json \ "latitude").as[Double],
+            (json \ "longitude").as[Double],
+            (json \ "datetime").as[String],
+            (json \ "temperature").as[Float],
+            (json \ "battery").as[Float]
+        ))
+    }
+
+    def msg = Action { request =>
+        request.body.asJson.map { json =>
+        json.validate[Drone].map { 
+          Drone => fixme = Drone :: fixme;
+            Ok("Proper Json");
+        }.recoverTotal{
+          e =>  println("Detected an error");
+                BadRequest("Detected error:");
+        }
+      }.getOrElse {
+        println("Completely failed");
+        BadRequest("Expecting Json data")
+      }
     }
 }
