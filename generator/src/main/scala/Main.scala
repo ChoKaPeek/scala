d7b88e8cad62bbda83b4557bf6019dbb0f64b494
Amael Tardif <amael.tardif@epita.fr>
2019-07-07 21:40:51 +0200

    generator to dataframes

--- generator/src/main/scala/Main.scala
+++ generator/src/main/scala/Main.scala
@@ -1,21 +1,22 @@
 import tools._
 import tools.Spark._
+import org.apache.spark.sql.functions._
+import spark.implicits._ // implicit encoder for case classes, uses spark object
 
 object Main extends App {
-    val population_path = "../data/population"
-    val objects = Parser.serialize(population_path)
-    Parser.serialize(population_path).map {
-        obj => Storage.write(obj)
+    Parser.serializePopulation("../data/population").foreach {
+        df => Storage.write(df)
     }
     
     val default = "../data/logs"
     val path2 = scala.io.StdIn.readLine(s"Please enter the path to json logs ($default)")
-    val stream = Parser.parse(path2, default)
-    stream.map {
-        elt => elt.map {
-            json => Post.sendToBase(json)
-        } 
+    val list = Parser.parse(path2, default)
+    list.map {
+        df => println(df); df.select(to_json(struct(df.col("*")))).as[String].foreachPartition {
+            Producer.writeIterToKafka(_)
+        }
     }
 
+    val ret = scala.io.StdIn.readChar()
     spark.stop
 }
