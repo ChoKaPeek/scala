622215fd5166e0838eef767dab69d446ceafd0e1
Amael Tardif <amael.tardif@epita.fr>
2019-07-08 10:06:11 +0200

    work on web service

--- handler/app/controllers/IndexController.scala
+++ handler/app/controllers/IndexController.scala
@@ -10,34 +10,45 @@ import play.api.libs.functional.syntax._
 import tools.Models._
 import tools.Overrides._
 import tools.Spark._
-import tools.Storage._
+import tools._
+
+import spark.implicits._
 
 class IndexController @Inject()(val controllerComponents: ControllerComponents)
-  extends BaseController {
+extends BaseController {
     def index() = Action {
         val user = Map("Undef" -> "N/A")
-        val data = readLog().toList
+        val data = Storage.readLog().toList
         Ok(views.html.hello("Welcome to DroneTech", user, data))
     }
 
     def list() = Action {
         val user = Map("Undef" -> "N/A")
-        val data = readLog().toList
+        val data = Storage.readLog().toList
         Ok(views.html.hello("Welcome to DroneTech", user, data))
     }
 
-    def msg() = Action { request =>
-        request.body.asJson.map { json =>
-        json.validate[Log].map { 
-          log => writeLog(Seq(log));
-                 Ok("Proper Json");
-        }.recoverTotal{
-          e =>  println("Detected an error");
-                BadRequest("Detected error:");
-        }
-      }.getOrElse {
-        println("Completely failed");
-        BadRequest("Expecting Json data")
-      }
+    def drones() = Action {
+        val user = Map("Undef" -> "N/A")
+        val data = Storage.readDrone().toList
+        Ok(views.html.drones("Our list of drones", user, data))
     }
+
+    // http method
+    def msg() = Action(parse.json) {
+        request => request.body.validate[Log].map { 
+            log => Storage.write(Seq(log).toDF); Ok("Proper Json")
+        }.recoverTotal {
+            e => println("Detected an error"); BadRequest("Detected error:")
+        }
+    }
+
+    def update() = Action {
+        Consumer.consumeFromKafka("logs")
+        val user = Map("Undef" -> "N/A")
+        val data = Storage.readLog().toList
+        Ok(views.html.hello("Welcome to DroneTech", user, data))
+    }
+  
+  
 }
