31c9f15863ca36f9be874880036b98a1550cdc2c
Amael Tardif <amael.tardif@epita.fr>
2019-07-08 10:00:56 +0200

    define encoders and implicits for models

--- generator/src/main/scala/tools/Models.scala
+++ generator/src/main/scala/tools/Models.scala
@@ -1,9 +1,37 @@
 package tools
 
-object Models {
-    case class Drone(id: Int, brand: String)
+import org.apache.spark.sql._
+import Spark._
 
-    case class Log(id: Int, id_drone: Int, speed: Float, altitude: Float,
-                   latitude: Double, longitude: Double, datetime: String,
-                   temperature: Float, battery: Float)
+import spark.implicits._
+
+object Models {
+    val logEncoder = Encoders.product[Log]
+    val droneEncoder = Encoders.product[Drone]
+
+    val logSchema = implicitly[Encoder[Log]].schema
+    val droneSchema = implicitly[Encoder[Drone]].schema
+
+    sealed trait Model {
+        def id: Int
+    }
+    
+    case class Drone
+    (
+        id: Int,
+        brand: String
+    ) extends Model
+
+    case class Log
+    (
+        id: Int,
+        id_drone: Int,
+        speed: Float,
+        altitude: Double,
+        latitude: Double,
+        longitude: Double,
+        datetime: String,
+        temperature: Float,
+        battery: Float
+    ) extends Model
 }
